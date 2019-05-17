b2639fc769ee9b264dd322fd39dab4c9695913fd
Samu Kumpulainen <samu.kumpulainen@revision.fi>
2019-05-17 15:14:12 +0300

    Test puzzle, validity check

--- sudoku.js
+++ sudoku.js
@@ -15,13 +15,49 @@ function createPuzzle() {
     puzzle.appendChild(table);
 }
 
-function addSubmit() {
+function addControls() {
     let body = document.getElementsByTagName('body')[0];
     let sub = document.createElement('button');
     sub.onclick = solvePuzzle;
     sub.textContent = "Solve"
-    body.appendChild(sub);
+    body.insertBefore(sub, document.getElementsByTagName("main")[0]);
+    let test = document.createElement('button');
+    test.onclick = createTestPuzzle;
+    test.textContent = "Fill in test puzzle"
+    body.insertBefore(test, document.getElementsByTagName("main")[0]);
+    
 }
+
+function createTestPuzzle() {
+
+    let puzzle1 = [
+        ["", 8, "", 7, "", 1, "", 3, ""],
+        [4, "", 9, "", "", "", "", "", ""],
+        ["", 5, "", "", 6, "", 4, 1, 8],
+        [7, "", "", "", "", 9, "", "", ""],
+        [8, "", "", 6, 1, "", 5, "", ""],
+        ["", 3, 5, "", "", "", "", 2, 9],
+        ["", 6, "", 4, "", 7, "", 9, ""],
+        [1, "", "", "", "", 8, "", "", 4],
+        ["", 2, "", "", 5, "", "", 7, ""]
+    ]
+
+    fillWithPuzzle(puzzle1);
+}
+
+
+function fillWithPuzzle(puzzleAsList) {
+    let cells = document.getElementsByTagName('td');
+    var arr = Array.prototype.slice.call( cells );
+    for (let i = 0; i < puzzleAsList.length; i++) {
+        for (let j = 0; j < puzzleAsList[i].length; j++) {
+            arr.shift().textContent = puzzleAsList[i][j];
+            
+        }
+        
+    }
+}
+
 
 function updateCounter(e) {
     let cell = e.target;
@@ -45,7 +81,7 @@ function extractSudoku() {
         let cells  = rows[i].getElementsByTagName('td');
         for (let j = 0; j < cells.length; j++) {
             let cellNumber = parseInt(cells[j].textContent);
-            let cellObject = {number: cellNumber, immutable: true, cellTarget: cells[j]};
+            let cellObject = {number: cellNumber, immutable: cellNumber ? true : false, cellTarget: cells[j]};
             rowObjects.push(cellObject);
         }
         contents.push(rowObjects);      
@@ -58,20 +94,33 @@ function extractSudoku() {
 function solvePuzzle() {
 
     let rows = extractSudoku();
-    
+    let justNumbers = rows.map( sub => sub.map( row => row.number));
+    console.log(justNumbers);
+    //testValidityCheck(justNumbers);
+
     let i = 0;
     while (i < rows.length) {
         let j = 0;
         while (j < rows[i].length) {
             let cell = rows[i][j];
             if (!cell.immutable) {
+                
                 let candidate = 1;
                 let isValid = false;
                 
-                while (candidate <= 9 && validWasFound) {
-                    cell.cellNumber = candidate;
-                    cell.cellTarget = candidate;
-                    isValid = checkValidity(rows, candidate, i, j);
+                // There is problem with backtracking :/
+                while (candidate <= 9 && !isValid) {
+                    isValid = checkValidity(justNumbers, candidate, i, j);
+                    if (isValid) {
+                        console.log(candidate);
+                        justNumbers[i][j] = candidate;
+                        cell.cellNumber = candidate;
+                        cell.cellTarget.textContent = candidate;
+                        break;
+                    } else {
+                        candidate++;
+                    }
+                
                 }
                 if (!isValid) {
                     if (0 < j) {
@@ -80,21 +129,53 @@ function solvePuzzle() {
                         i--;
                         break;
                     }
-                }
-                
-                
-                
+                }   
             }
-            
             j++;
         }
         i++;
     }
 }
 
+function testValidityCheck(rows) {
 
-function checkValidity(puzzle, value, iy, ix) {
+    let value = 7;
+    let y = 2;
+    let x = 4;
+    let results = checkValidity(rows, value, y, x);
+    console.log(results);
     
+}
+
+
+function checkValidity(rows, value, iy, ix) {
+    
+    //check row
+    if (0 <= rows[iy].indexOf(value)) {
+        return false;
+    }
+    //check column
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i][ix] === value) {
+            return false;
+        }    
+    } 
+    //check small square
+    let sx = Math.floor(ix / 3);
+    let sy = Math.floor(iy / 3);
+
+    let start_x = 3 * sx;
+    let start_y = 3 * sy;
+
+    for (let i = start_y; i < start_y + 3; i++) {
+        for (let j = start_x; j < start_x + 3; j++) {
+            if (rows[i][j] === value) {
+                return false;
+            }
+        }
+    }
+    return true;
+
 }
 
 
@@ -102,7 +183,8 @@ function checkValidity(puzzle, value, iy, ix) {
 
 function main() {
     createPuzzle();
-    addSubmit();
+    addControls();
+    
 }
 
 
